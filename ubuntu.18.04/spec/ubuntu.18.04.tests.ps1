a28252829b6e8b2a4d501479c9833de8b31be677
Geoff Lamrock <geoff.lamrock@gmail.com>
2022-03-14 15:00:17 +1100

    Fixes test

--- ubuntu.18.04/spec/ubuntu.18.04.tests.ps1
+++ ubuntu.18.04/spec/ubuntu.18.04.tests.ps1
@@ -18,7 +18,7 @@ Describe  'installed dependencies' {
     }
 
     It 'has java installed' {
-        java --version | Should -beLike "*11.0.13*"
+        java --version | Should -beLike "*11.0.14*"
         $LASTEXITCODE | Should -be 0
     }
 
